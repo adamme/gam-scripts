f8c1ecd63bc8ab15a81f89748297372d90925f17
Adamme <adammekearney@gmail.com>
2019-06-13 19:04:15 -0700

    Update get_license_count.sh

--- get_license_count.sh
+++ get_license_count.sh
@@ -8,4 +8,6 @@ usercount=$($gam info domain | grep 'G Suite Enterprise Users' | tr -cd '0-9')
 
 #calculate and print available G Suite user licenses, then store in txt file
 licensecount=$(expr $licensemax - $usercount)
-echo $licensecount " G Suite User Licenses Available" > user_license_count.txt
+echo $licensecount " G Suite User Licenses Available"
+
+curl -X POST -H 'Content-type: application/json' --data "$(eval "echo "'{\"text\": \"$licensecount G Suite User Licenses Available\"}'"")" https://hooks.slack.com/services/######/###################
