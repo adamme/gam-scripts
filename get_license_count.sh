aaf7f90b39e50d5ca2cc9c2f6fc427fc45f4f827
Adamme <adammekearney@gmail.com>
2020-11-19 18:13:24 -0800

    Update get_license_count.sh

--- get_license_count.sh
+++ get_license_count.sh
@@ -2,12 +2,12 @@
 
 gam="sudo $HOME/bin/gam/gam"
 
-#find license max and user count for G Suite Enterprise Licenses
-licensemax=$($gam info domain | grep 'G Suite Enterprise Licenses' | tr -cd '0-9')
-usercount=$($gam info domain | grep 'G Suite Enterprise Users' | tr -cd '0-9')
+#find license max and user count for Google Workspace Enterprise Licenses
+licensemax=$($gam info domain | grep 'Google Workspace Enterprise Plus Licenses' | tr -cd '0-9')
+usercount=$($gam info domain | grep 'Google Workspace Enterprise Plus Users' | tr -cd '0-9')
 
 #calculate and print available G Suite user licenses, then store in txt file
 licensecount=$(expr $licensemax - $usercount)
-echo $licensecount " G Suite User Licenses Available"
+echo $licensecount " Google Workspace User Licenses Available"
 
-curl -X POST -H 'Content-type: application/json' --data "$(eval "echo "'{\"text\": \"$licensecount G Suite User Licenses Available\"}'"")" https://hooks.slack.com/services/######/###################
+curl -X POST -H 'Content-type: application/json' --data "$(eval "echo "'{\"text\": \"$licensecount Google Workspace User Licenses Available\"}'"")" https://hooks.slack.com/services/######/###################
